6ae961a401deaf37c1f7fbb1caf02b1db670aa75
Adam Wójs <adam@wojs.pl>
2017-11-15 22:15:11 +0100

    Impl. exeception converter for URL gateway

---
+++ src/lib/Core/Persistence/Legacy/URL/Gateway/ExceptionConversion.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace EzSystems\EzPlatformLinkManager\Core\Persistence\Legacy\URL\Gateway;
+
+use EzSystems\EzPlatformLinkManager\API\Repository\Values\Query\Criterion;
+use EzSystems\EzPlatformLinkManager\Core\Persistence\Legacy\URL\Gateway;
+use EzSystems\EzPlatformLinkManager\SPI\Persistence\URL\URL;
+use Doctrine\DBAL\DBALException;
+use PDOException;
+use RuntimeException;
+
+class ExceptionConversion extends Gateway
+{
+    /**
+     * The wrapped gateway.
+     *
+     * @var Gateway
+     */
+    protected $innerGateway;
+
+    /**
+     * ExceptionConversion constructor.
+     *
+     * @param \EzSystems\EzPlatformLinkManager\Core\Persistence\Legacy\URL\Gateway $innerGateway
+     */
+    public function __construct(Gateway $innerGateway)
+    {
+        $this->innerGateway = $innerGateway;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function insertUrl(URL $url)
+    {
+        try {
+            return $this->innerGateway->insertUrl($url);
+        } catch (DBALException $e) {
+            throw new RuntimeException('Database error', 0, $e);
+        } catch (PDOException $e) {
+            throw new RuntimeException('Database error', 0, $e);
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function updateUrl(URL $url)
+    {
+        try {
+            return $this->innerGateway->updateUrl($url);
+        } catch (DBALException $e) {
+            throw new RuntimeException('Database error', 0, $e);
+        } catch (PDOException $e) {
+            throw new RuntimeException('Database error', 0, $e);
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function find(Criterion $criterion, $offset, $limit, $doCount = true)
+    {
+        try {
+            return $this->innerGateway->find($criterion, $offset, $limit, $doCount);
+        } catch (DBALException $e) {
+            throw new RuntimeException('Database error', 0, $e);
+        } catch (PDOException $e) {
+            throw new RuntimeException('Database error', 0, $e);
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function loadUrlData($id)
+    {
+        try {
+            return $this->innerGateway->loadUrlData($id);
+        } catch (DBALException $e) {
+            throw new RuntimeException('Database error', 0, $e);
+        } catch (PDOException $e) {
+            throw new RuntimeException('Database error', 0, $e);
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getRelatedContentIds($id)
+    {
+        try {
+            return $this->innerGateway->getRelatedContentIds($id);
+        } catch (DBALException $e) {
+            throw new RuntimeException('Database error', 0, $e);
+        } catch (PDOException $e) {
+            throw new RuntimeException('Database error', 0, $e);
+        }
+    }
+}
