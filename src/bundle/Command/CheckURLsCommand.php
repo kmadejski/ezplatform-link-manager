39be17bffff6adba5c67951eb28bd9ffe304e3d2
Adam Wójs <adam@wojs.pl>
2017-12-07 07:13:54 +0100

    Impl. pagination support in the command for urls validation

--- src/bundle/Command/CheckURLsCommand.php
+++ src/bundle/Command/CheckURLsCommand.php
@@ -2,21 +2,34 @@
 
 namespace EzSystems\EzPlatformLinkManagerBundle\Command;
 
-use EzSystems\EzPlatformLinkManager\API\Repository\Values\URLQuery;
 use EzSystems\EzPlatformLinkManager\API\Repository\Values\Query\Criterion;
-use EzSystems\EzPlatformLinkManager\URLChecker\URLCheckerInterface;
+use EzSystems\EzPlatformLinkManager\API\Repository\Values\Query\SortClause;
+use EzSystems\EzPlatformLinkManager\API\Repository\Values\URLQuery;
+use RuntimeException;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class CheckURLsCommand extends ContainerAwareCommand
 {
+    const DEFAULT_ITERATION_COUNT = 50;
+
     /**
      * {@inheritdoc}
      */
     public function configure()
     {
         $this->setName('ezplatform:check-urls');
+        $this->setDescription('Checks validity of external URLs');
+        $this->addOption(
+            'iteration-count',
+            'c',
+            InputOption::VALUE_OPTIONAL,
+            'Number of urls to be checked in a single iteration, for avoiding using too much memory',
+            self::DEFAULT_ITERATION_COUNT
+        );
     }
 
     /**
@@ -24,12 +37,48 @@ class CheckURLsCommand extends ContainerAwareCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $query = new URLQuery();
-        $query->filter = new Criterion\MatchAll();
-        $query->limit = -1;
+        $limit = $input->getOption('iteration-count');
+        if (!is_numeric($limit) || (int)$limit < 1) {
+            throw new RuntimeException("'--iteration-count' option should be > 0, got '{$limit}'");
+        }
 
-        /** @var URLCheckerInterface $urlChecker */
-        $urlChecker = $this->getContainer()->get('ezpublish.url_checker');
-        $urlChecker->check($query);
+        $query = new URLQuery();
+        $query->filter = new Criterion\VisibleOnly();
+        $query->sortClauses = [
+            new SortClause\URL(),
+        ];
+        $query->offset = 0;
+        $query->limit = $limit;
+
+        $totalCount = $this->getTotalCount(clone $query);
+
+        $progress = new ProgressBar($output, $totalCount);
+        $progress->start();
+        while ($query->offset < $totalCount) {
+            $this->getUrlHandler()->check($query);
+            $progress->advance(min($limit, $totalCount - $query->offset));
+            $query->offset += $limit;
+        }
+        $progress->finish();
+    }
+
+    private function getTotalCount(URLQuery $query)
+    {
+        $repository = $this->getContainer()->get('ezpublish.api.repository');
+        $urlService = $this->getContainer()->get('ezpublish.api.service.url');
+
+        $query->limit = 0;
+
+        return $repository->sudo(function () use ($query, $urlService) {
+            return $urlService->findUrls($query)->count;
+        });
+    }
+
+    /**
+     * @return \EzSystems\EzPlatformLinkManager\URLChecker\URLCheckerInterface
+     */
+    private function getUrlHandler()
+    {
+        return $this->getContainer()->get('ezpublish.url_checker');
     }
 }
