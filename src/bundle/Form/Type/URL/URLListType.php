57b4805ee1c005b31aba3b1527b64a8fb0128639
Kamil Madejski <kamil.madejski@ez.no>
2017-12-11 16:06:39 +0100

    Removed unused use statement

--- src/bundle/Form/Type/URL/URLListType.php
+++ src/bundle/Form/Type/URL/URLListType.php
@@ -7,7 +7,6 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\SearchType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Translation\TranslatorInterface;
